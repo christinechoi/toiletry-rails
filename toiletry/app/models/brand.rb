a86d89e3ea4773a455b35770751750d4e66419d4
christinechoi <smi77y@gmail.com>
2017-04-16 16:15:46 -0400

    Add nested form in collection/new to add item

--- toiletry/app/models/brand.rb
+++ toiletry/app/models/brand.rb
@@ -1,4 +1,9 @@
 class Brand < ApplicationRecord
   has_many :items
   has_many :collections, through: :items
+
+
+  # def item_name=(name)
+  #    self.item = Item.find_or_create_by(name: name)
+  # end
 end
--- toiletry/app/models/item.rb
+++ toiletry/app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :brand
   belongs_to :collection
+
+
+  # def brand_name=(name)
+  #    self.brand = Item.find_or_create_by(name: name)
+  # end
 end
