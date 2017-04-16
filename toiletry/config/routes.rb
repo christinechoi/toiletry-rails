d3cb8eec6d2d7638acfd3e9a95b0b812761def2f
christinechoi <smi77y@gmail.com>
2017-04-15 22:05:52 -0400

    Create controllers for Item, Brand, and Collection

--- toiletry/config/routes.rb
+++ toiletry/config/routes.rb
@@ -1,4 +1,46 @@
 Rails.application.routes.draw do
+  get 'collection/index'
+
+  get 'collection/new'
+
+  get 'collection/create'
+
+  get 'collection/show'
+
+  get 'collection/edit'
+
+  get 'collection/update'
+
+  get 'collection/destroy'
+
+  get 'brand/index'
+
+  get 'brand/new'
+
+  get 'brand/create'
+
+  get 'brand/show'
+
+  get 'brand/edit'
+
+  get 'brand/update'
+
+  get 'brand/destroy'
+
+  get 'item/index'
+
+  get 'item/new'
+
+  get 'item/create'
+
+  get 'item/show'
+
+  get 'item/edit'
+
+  get 'item/update'
+
+  get 'item/destroy'
+
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   get 'home/index'
 
