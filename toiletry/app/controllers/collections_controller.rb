e44f70aa1474ab830a006e68b220537fa3a89803
christinechoi <smi77y@gmail.com>
2017-04-16 13:50:17 -0400

    Create form for new collection, collections#new, collections#show

--- toiletry/app/controllers/collections_controller.rb
+++ toiletry/app/controllers/collections_controller.rb
@@ -4,12 +4,23 @@ class CollectionsController < ApplicationController
   end
 
   def new
+    @collection = Collection.new
   end
 
   def create
+
+    @collection = Collection.new(collection_params)
+    
+    if @collection.save
+      # binding.pry
+      redirect_to collection_path(@collection)
+    else
+      render 'new'
+    end
   end
 
   def show
+     @collection = Collection.find(params[:id])
   end
 
   def edit
@@ -20,4 +31,14 @@ class CollectionsController < ApplicationController
 
   def destroy
   end
+
+
+private
+
+def collection_params
+  params.require(:collection).permit(:title, :user_id)
+end
+
+
+
 end
