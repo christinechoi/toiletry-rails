3aad1a01f1e766b810929f1a6fa3397720867034
christinechoi <smi77y@gmail.com>
2017-07-17 21:50:43 -0400

    Add code to build associated object and whitelist attributes

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -9,10 +9,17 @@ class CollectionsController < ApplicationController
   end
 
   def create
+    binding.pry
     @collection = current_user.collections.new(title: params[:collection][:title])
+    @item = @collection.items.build(brand: params[:collection][:items_attributes]["0"][:brand], description: params[:collection][:items_attributes]["0"][:description], name: params[:collection][:items_attributes]["0"][:name], :id => params[:collection][:items_attributes]["0"][:id], user: current_user)#, collection_id: @collection.id)
+    @item.save
+    binding.pry
     if @collection.save
+      binding.pry
+
       redirect_to collection_path(@collection)
     else
+      binding.pry
       flash[:error] = "Sorry, please try again."
       redirect_to new_collection_path
     end
@@ -33,6 +40,6 @@ class CollectionsController < ApplicationController
   private
 
   def collection_params
-    params.require(:collection).permit(:title, :user_id)
+    params.require(:collection).permit(:title, :user_id, items_attributes: [:id, :name, :brand, :description])
   end
 end
