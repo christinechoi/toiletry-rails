68204aca1b9a305982b3955e0d34f0181aef7ab1
christinechoi <smi77y@gmail.com>
2017-08-26 18:50:56 -0400

    Update associations and custom attribute writer

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,13 +1,15 @@
 class Collection < ApplicationRecord
-  has_many :items, inverse_of: :collection, dependent: :destroy
+  has_many :items, dependent: :destroy
   has_many :users, through: :items
   validates :title, presence: true
+
+  accepts_nested_attributes_for :items
 
   def items_attributes=(item_attributes)
     binding.pry
     item_attributes.values.each do |item_attribute|
-      item = item.find_or_create_by(item_attribute)
-      self.items << item
+      @item = self.items.build(item_attribute)
+      binding.pry
     end
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :collection, optional: true
-  belongs_to :user
+  belongs_to :user, inverse_of: :items
   validates :brand, presence: true
   validates :name, presence: true
+
+  # accepts_nested_attributes_for :collection
 
   def self.by_brand(brand)
     where(brand: brand)
